15d417f74aaf8e0f8c20c908b8bef5a402f97a31
MrsMetaMorfa <maitressse.metamorfa@gmail.com>
2020-05-09 19:53:36 +0500

    final

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (document.querySelector('.compilation')) {
       let itemsOnPage = document.querySelectorAll('.item'), itemWidth, imageHeight;
-      if ((window.innerWidth < 1024) && (window.innerWidth < window.innerHeight)) {
+      if ((window.innerWidth <= 1024) && (window.innerWidth < window.innerHeight)) { //portrait
         itemWidth = itemsOnPage[0].querySelector('.item_image').offsetWidth;
         imageHeight = (itemWidth) / 141 * 212;
         Array.prototype.forEach.call(itemsOnPage, function(elem){
@@ -26,23 +26,31 @@ document.addEventListener('DOMContentLoaded', function() {
     if (document.querySelector('.article')) {
       let articleImage = document.querySelector('.article_image'),
           imageHeight, imageWidth;
-      if ( (window.innerWidth < 1024) && (window.innerWidth < window.innerHeight) ) {
+      if ( (window.innerWidth <= 1024) && (window.innerWidth < window.innerHeight) ) { //portrait
         articleImage.style.width = 'inherit';
         articleImage.style.height = 'inherit';
         imageWidth = articleImage.offsetWidth;
         imageHeight = articleImage.offsetHeight;
         console.log(imageHeight, imageWidth);
-        articleImage.nextElementSibling.style.left = 0;
+        articleImage.nextElementSibling.style.paddingLeft = 0;
         articleImage.style.height = imageHeight + 'px';
         articleImage.nextElementSibling.querySelector('.header').style.marginBottom = imageHeight + 'px';
-      } else {
+      } else if ((window.innerWidth > 1024) && (window.innerWidth > window.innerHeight)) { //landscape
         articleImage.style.width = 'inherit';
         articleImage.style.height = 'inherit';
         imageHeight = articleImage.offsetHeight;
         imageWidth = (imageHeight) / 212 * 141;
         articleImage.nextElementSibling.querySelector('.header').style.marginBottom = 'inherit';
         articleImage.style.width = imageWidth + 'px';
-        articleImage.nextElementSibling.style.left = imageWidth + 'px';
+        articleImage.nextElementSibling.style.paddingLeft = (imageWidth + 64) + 'px';
+      } else {
+        articleImage.style.width = 'inherit';
+        articleImage.style.height = 'inherit';
+        imageHeight = articleImage.offsetHeight;
+        imageWidth = articleImage.offsetWidth;
+        articleImage.nextElementSibling.querySelector('.header').style.marginBottom = 'inherit';
+        articleImage.style.width = imageWidth + 'px';
+        articleImage.nextElementSibling.style.paddingLeft = (imageWidth + 48) + 'px';
       }
     }
     if (document.querySelector('#copyLink')) {
